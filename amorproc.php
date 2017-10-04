39cc90b567f11989ecbf6b5118009dbc24e2a914
kevinfgranadoss <30881031+kevinfgranadoss@users.noreply.github.com>
2017-10-04 00:22:24 -0500

    Add files via upload

---
+++ amorproc.php
@@ -0,0 +1,3 @@
+<?php
+echo $_REQUEST["novio"]." ama a ".$_REQUEST["novia"];
+?>
---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Amor Web</title>
+    </head>
+    <body>
+        <form action="amorproc.php">
+            <p>Novio:</p>
+            <input type="text" name="novio">
+            <p>Novia:</p>
+            <input type="text" name="novia">
+            <input type="submit" name="acc" value="Enamorar">
+        </form>
+    </body>
+</html>
